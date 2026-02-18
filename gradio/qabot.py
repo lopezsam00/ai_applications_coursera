62f94f90e77b0079287393539765d894d70c4d2a
sam <lopezsam00@outlook.com>
2026-02-18 08:18:12 -0800

    simliarity search

--- gradio/qabot.py
+++ gradio/qabot.py
@@ -18,16 +18,20 @@ load_dotenv()
 
 def warn(*args, **kwargs):
     pass
+
+
 import warnings
+
 warnings.warn = warn
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
+
 
 def get_llm():
-    model_id = 'ibm/granite-3-2-8b-instruct'
+    model_id = "ibm/granite-3-2-8b-instruct"
 
     parameters = {
-    GenParams.MAX_NEW_TOKENS: 256,  # this controls the maximum number of tokens in the generated output
-    GenParams.TEMPERATURE: 0.2, # this randomness or creativity of the model's responses 
+        GenParams.MAX_NEW_TOKENS: 256,  # this controls the maximum number of tokens in the generated output
+        GenParams.TEMPERATURE: 0.2,  # this randomness or creativity of the model's responses
     }
 
     url = os.getenv("IBM_URL_END_POINT")
@@ -39,23 +43,24 @@ def get_llm():
         url=url,
         project_id=project_id,
         params=parameters,
-        apikey=apikey
+        apikey=apikey,
     )
     return watsonx_llm
+
 
 def document_loader(file):
     loader = PyPDFLoader(file)
     loaded_document = loader.load()
     return loaded_document
 
+
 def text_splitter(data):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=0,
-        length_function=len
+        chunk_size=1000, chunk_overlap=0, length_function=len
     )
     chunks = text_splitter.split_documents(data)
     return chunks
+
 
 def watsonx_embedding():
     url = os.getenv("IBM_URL_END_POINT")
@@ -64,21 +69,23 @@ def watsonx_embedding():
 
     embed_params = {
         EmbedTextParamsMetaNames.TRUNCATE_INPUT_TOKENS: 3,
-        EmbedTextParamsMetaNames.RETURN_OPTIONS: {"input_text": True}
+        EmbedTextParamsMetaNames.RETURN_OPTIONS: {"input_text": True},
     }
     watsonx_embedding = WatsonxEmbeddings(
         model_id="ibm/slate-125m-english-rtrvr-v2",
         url=url,
         project_id=project_id,
         params=embed_params,
-        apikey=apikey
+        apikey=apikey,
     )
     return watsonx_embedding
+
 
 def vector_database(chunks):
     embedding_model = watsonx_embedding()
     vectordb = Chroma.from_documents(chunks, watsonx_embedding())
     return vectordb
+
 
 def retriever(file):
     splits = document_loader(file)
@@ -87,6 +94,7 @@ def retriever(file):
     retriever = vectordb.as_retriever()
     return retriever
 
+
 def retriever_qa(file, query):
     llm = get_llm()
     retriever_obj = retriever(file)
@@ -94,19 +102,27 @@ def retriever_qa(file, query):
         llm=llm,
         chain_type="stuff",
         retriever=retriever_obj,
-        return_source_documents=True
+        return_source_documents=True,
     )
     response = qa.invoke(input=query)
-    return response['result']
+    return response["result"]
+
 
 rag_application = gr.Interface(
     fn=retriever_qa,
-        inputs=[
-        gr.File(label="Upload PDF File", file_count="single", file_types=['.pdf'], type="filepath"),  # Drag and drop file upload
-        gr.Textbox(label="Input Query", lines=2, placeholder="Type your question here...")
+    inputs=[
+        gr.File(
+            label="Upload PDF File",
+            file_count="single",
+            file_types=[".pdf"],
+            type="filepath",
+        ),  # Drag and drop file upload
+        gr.Textbox(
+            label="Input Query", lines=2, placeholder="Type your question here..."
+        ),
     ],
     outputs=gr.Textbox(label="Answer"),
     title="RAG Bot with IBM Watsonx",
-    description="Upload a PDF document and ask questions about its content. Powered by IBM Watsonx"
+    description="Upload a PDF document and ask questions about its content. Powered by IBM Watsonx",
 )
 rag_application.launch()
